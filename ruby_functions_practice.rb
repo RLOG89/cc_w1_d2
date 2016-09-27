c69c5ae9cc2091538a5b0829b60816b42bd4e422
RLOG89 <ross.loggie@gmail.com>
2016-09-27 15:59:26 +0100

    number_to_full_month_name successfully tested

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -29,3 +29,34 @@ end
 def add_string_as_number( str_1, str_2 )
   return ( str_1.to_i + str_2.to_i )
 end
+
+def number_to_full_month_name( num )
+  months ={ 
+    1 => "January",
+    2 => "February",
+    3 => "March",
+    4 => "April",
+    5 => "May",
+    6 => "June",
+    7 => "July",
+    8 => "August",
+    9 => "September",
+    10 => "October",
+    11 => "November",
+    12 => "December"
+  }
+return months[num]
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
